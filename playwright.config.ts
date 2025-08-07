9ee0e756d8022eaddcdf4d25aab59d71a59e4a44
Vladyslav Lyshchuk <v.lyshchuk@creatio.com>
2025-08-07 15:30:11 +0200

    feat: Add Chromium and WebKit projects to Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,9 +36,21 @@ export default defineConfig({
 
   projects: [
     {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome']
+      },
+    },
+    {
       name: 'firefox',
       use: { 
         ...devices['Desktop Firefox']
+      },
+    },
+    {
+      name: 'webkit',
+      use: { 
+        ...devices['Desktop Safari']
       },
     },
   ],
